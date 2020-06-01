d829bdccf408ed1e855fd4451379c2645712a56b
Corban Miller <ac94553@avcol.school.nz>
2020-06-02 10:56:37 +1200

    First commit

--- main.Designer.cs
+++ main.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace anti_cheat
 {
-    partial class Form1
+    partial class main
     {
         /// <summary>
         /// Required designer variable.
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(484, 341);
+            this.Name = "main";
+            this.Text = "Anti-Cheat";
+            this.Load += new System.EventHandler(this.main_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
